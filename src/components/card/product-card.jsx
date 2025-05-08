7b768caf575a30de9db6b94abd2f4720312eb0a6
Kimball Oyler <167648368+koyler88@users.noreply.github.com>
2025-05-07 18:19:22 -0600

    Fix product price decimals

--- src/components/card/product-card.jsx
+++ src/components/card/product-card.jsx
@@ -5,7 +5,7 @@ function ProductCard({ url, price, title, onClick, id }) {
       <div className="product-card" data-id={id}>
         <img className="product-img" src={url} alt="" />
         <p className="product-title">{title}</p>
-        <p className="price-tag">${price}</p>
+        <p className="price-tag">${price.toFixed(2)}</p>
         <button onClick={onClick} className="add-to-cart">
           Add to Cart
         </button>
